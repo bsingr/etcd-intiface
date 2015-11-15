0b556cb44d1c0ffebd876fdc6e25f5e6f7cafdad
Jens Bissinger <mail@jens-bissinger.de>
2015-11-15 09:40:02 +0100

    Expose etcdjs via rpc; add bower and setup client

--- api.js
+++ api.js
@@ -1,38 +1,30 @@
 'use babel'
 
 import koa from 'koa'
-import route from 'koa-route'
+import koaRouterLib from 'koa-router'
+import koaBodyLib from 'koa-body'
 import etcdjs from 'etcdjs'
 
-let routes = {
-  get: function *() {
-    let that = this
-    let store = etcdjs('127.0.0.1:4001')
-    yield function(callback) {
-      store.get('hello', function(err, result) {
-        that.body = result
-        callback()
-      })
-    }
-  },
-  set: function *() {
-    let that = this
-    let store = etcdjs('127.0.0.1:4001')
-    // console.log(this.body)
-    yield function(callback) {
-      store.set('hello', 'world', function(err, result) {
-        that.body = result
-        callback()
-      })
-    }
+let koaRouter = koaRouterLib()
+let koaBody = koaBodyLib()
+
+koaRouter.post('/etcd', koaBody, function *(next) {
+  let that = this
+  let etcdCall = that.request.body
+  let store = etcdjs(etcdCall.url)
+  yield function(callback) {
+    let method = store[etcdCall.method]
+    let params = store[etcdCall.params] || []
+    params.push(function(err, result) {
+      that.body = result
+      callback()
+    })
+    method.apply(store, params)
   }
-}
+})
 
 export default function () {
   let app = koa()
-
-  app.use(route.get('/get', routes.get))
-  app.use(route.get('/set', routes.set))
-
+  app.use(koaRouter.routes())
   return app
 }
